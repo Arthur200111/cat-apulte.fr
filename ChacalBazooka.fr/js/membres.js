be2f377e34808a3021e59d6678a3ebf9f26c4800
Arthur200111 <50994526+Arthur200111@users.noreply.github.com>
2021-11-20 23:52:05 +0100

    Ajout page membres + allos + programme
    Pour accéder aux pages membres et allos il faut créer un serveur en passant par le shell avec la commande python -m http.server 1111 et après mettre l'url localhost:1111 dans un navigateur (google,firefox,...). 1111 peut être remplacé par n'importe quel nombre, il faut juste être cohérent.
    Pour plus d'info : 
     - https://stackoverflow.com/questions/17351016/set-up-python-simplehttpserver-on-windows
     - https://www.educative.io/edpresso/how-to-add-python-to-path-variable-in-windows

---
+++ ChacalBazooka.fr/js/membres.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    console.log('ready');
+    $.ajax({
+        url:"../data/membres/membres.csv",
+        dataType:"text",
+        success:function(data){
+            var employee_data = data.split(/\r?\n|\r/);
+            var membres ='';
+            for(var count = 0; count<employee_data.length; count++){
+                membres += '<div class = "flip flip-vertical">';
+                var cell_data = employee_data[count].split("|");
+                membres += '<div class="front" style="background-image: url(../data/membres/' + cell_data[0] + '_' + cell_data[1] + '.jpg)">';
+                membres += '<h1 class="text-shadow">' + cell_data[2] + '</h1>';
+                membres += '</div>';
+
+                membres += '<div class = "back">';
+                membres += '<h2>' + cell_data[0] + ' ' + cell_data[1] + '</h2>';
+                membres += '<p>' + cell_data[3] + '</p>';
+                membres += '</div>';
+
+                membres += '</div>';
+            }
+            $('#container').html(membres);
+        }
+    });
+});
